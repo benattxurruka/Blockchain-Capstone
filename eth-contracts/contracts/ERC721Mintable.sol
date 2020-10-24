65ee827829355caa7bcc23187863b8a1958a6c07
btxurruka <betxurruka@gmail.com>
2020-10-24 12:22:44 +0200

    Fill Ownable contract

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -8,17 +8,34 @@ import "./Oraclize.sol";
 
 contract Ownable {
     //  TODO's
+    Address ozAddress;
     //  1) create a private '_owner' variable of type address with a public getter function
-    //  2) create an internal constructor that sets the _owner var to the creater of the contract 
-    //  3) create an 'onlyOwner' modifier that throws if called by any account other than the owner.
-    //  4) fill out the transferOwnership function
-    //  5) create an event that emits anytime ownerShip is transfered (including in the constructor)
+    private address _owner;
 
+    function getOwner() public view returns (address) {
+        return _owner;
+    }
+    //  2) create an internal constructor that sets the _owner var to the creater of the contract 
+    constructor () internal {
+        _owner = msg.sender;
+        emit NewOwner(_owner);
+    }
+    //  3) create an 'onlyOwner' modifier that throws if called by any account other than the owner.
+    modifier onlyOwner() {
+        require(msgn.sender == _owner, "Caller is not contract owner");
+        _;
+    }
+    //  4) fill out the transferOwnership function
     function transferOwnership(address newOwner) public onlyOwner {
         // TODO add functionality to transfer control of the contract to a newOwner.
         // make sure the new owner is a real address
+        require(!ozAddress.isContract(msg.sender()), "The given address is a contract address and it should be an account address.");
+        _owner = msg.sender;
+        emit NewOwner(_owner);
 
     }
+    //  5) create an event that emits anytime ownerShip is transfered (including in the constructor)
+    event NewOwner(address owner);
 }
 
 //  TODO's: Create a Pausable contract that inherits from the Ownable contract
